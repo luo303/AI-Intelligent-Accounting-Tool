bf18642bf1cfdc542fd9fbdbdb7149b29991c7cd
xhh <jiatangxi@qq.com>
2025-10-12 23:16:59 +0800

    'eslint与prettier集成'

--- eslint.config.ts
+++ eslint.config.ts
@@ -1,18 +1,39 @@
 import { globalIgnores } from 'eslint/config'
-import { defineConfigWithVueTs, vueTsConfigs } from '@vue/eslint-config-typescript'
+import {
+  defineConfigWithVueTs,
+  vueTsConfigs
+} from '@vue/eslint-config-typescript'
 import pluginVue from 'eslint-plugin-vue'
 import skipFormatting from '@vue/eslint-config-prettier/skip-formatting'
-
+import prettierPlugin from 'eslint-plugin-prettier'
+import prettierconfig from 'eslint-config-prettier'
 
 export default defineConfigWithVueTs(
   {
     name: 'app/files-to-lint',
-    files: ['**/*.{ts,mts,tsx,vue}'],
+    files: ['**/*.{ts,mts,tsx,vue}']
   },
 
   globalIgnores(['**/dist/**', '**/dist-ssr/**', '**/coverage/**']),
 
+  // 引入Prettier插件（关键步骤）
+  {
+    plugins: {
+      prettier: prettierPlugin
+    }
+  },
+  prettierconfig,
   pluginVue.configs['flat/essential'],
   vueTsConfigs.recommended,
   skipFormatting,
+
+  // 自定义规则
+  {
+    rules: {
+      // 允许使用 any 类型
+      '@typescript-eslint/no-explicit-any': 'off',
+
+      'prettier/prettier': 'warn'
+    }
+  }
 )
